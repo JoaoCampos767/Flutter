30c24b12bcb3c74bdf4f9954fde06216d77b01a9
JoaoCamposMBM <joao.pedro@mbmsolutions.com.br>
2022-10-31 07:55:48 -0300

    cara ou coroa

---
+++ cara_coroa/lib/jogar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  @override
+  State<Jogar> createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff39004d),
+      body: Container(
+          child: Column(
+        children: [],
+      )),
+    );
+  }
+}
