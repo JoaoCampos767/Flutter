f20e192d2119fb434ccbdaf67314111154ed7526
JoaoCamposMBM <joao.pedro@mbmsolutions.com.br>
2022-10-31 09:15:56 -0300

    finalizando o app cara ou coroa

--- cara_coroa/lib/jogar.dart
+++ cara_coroa/lib/jogar.dart
@@ -1,4 +1,6 @@
+import 'package:cara_coroa/resultado.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Jogar extends StatefulWidget {
   @override
@@ -6,13 +8,30 @@ class Jogar extends StatefulWidget {
 }
 
 class _JogarState extends State<Jogar> {
+  void _exibirResultado() {
+    var itens = ["cara", "coroa"];
+    var number = Random().nextInt(itens.length);
+    var result = itens[number];
+
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => Resultado(result)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff39004d),
+      backgroundColor: Color(0xff61bd86),
       body: Container(
           child: Column(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset("images/logo.png"),
+          GestureDetector(
+            onTap: _exibirResultado,
+            child: Image.asset("images/botao_jogar.png"),
+          )
+        ],
       )),
     );
   }
