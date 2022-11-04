e68679b2fa39304f0e9672026ba501bc20637bb7
JoaoCamposMBM <joao.pedro@mbmsolutions.com.br>
2022-11-04 08:49:17 -0300

    Consumo de servico avancado

---
+++ consumo_servico_avanc/lib/main.dart
@@ -0,0 +1,9 @@
+// @dart=2.9
+import 'package:consumo_servico_avanc/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
