4e51513e97e716bf21a14b6ea2b0b7ff4a0d8c52
JoaoCamposMBM <joao.pedro@mbmsolutions.com.br>
2022-11-06 14:43:44 -0300

    Aplicando post, put, path, delete

--- consumo_servico_avanc/lib/home_page.dart
+++ consumo_servico_avanc/lib/home_page.dart
@@ -21,10 +21,56 @@ class _HomePageState extends State<HomePage> {
 
     for (var post in dadosJson) {
       Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
-      postagens.add( p );
+      postagens.add(p);
     }
 
     return postagens;
+  }
+
+  _post() async {
+    Post post = Post(112, 0, "Titulo", "Corpo da postagem");
+
+    var corpo = jsonEncode(post.toJson());
+
+    var response = await http.post(_urlBase + "/posts",
+        headers: {"Content-type": "application/json; charset=UTF-8"},
+        body: corpo);
+
+    print(response.statusCode);
+    print(response.body);
+  }
+
+  _put() async {
+    Post put = Post(102, 0, "Titulo alterado", "Corpo da postagem 2");
+
+    var corpo = jsonEncode(put.toJson());
+
+    var response = await http.put(_urlBase + "/posts/2",
+        headers: {"Content-type": "application/json; charset=UTF-8"},
+        body: corpo);
+
+    print(response.statusCode);
+    print(response.body);
+  }
+
+  _path() async {
+    Post path = Post(102, 0, "", "Corpo da postagem 2");
+
+    var corpo = jsonEncode(path.toJson());
+
+    var response = await http.patch(_urlBase + "/posts/2",
+        headers: {"Content-type": "application/json; charset=UTF-8"},
+        body: corpo);
+
+    print(response.statusCode);
+    print(response.body);
+  }
+
+  _delete() async {
+    var response = await http.delete(_urlBase + "/posts/1");
+
+    print(response.statusCode);
+    print(response.body);
   }
 
   @override
@@ -33,45 +79,62 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Text("Consumo de servico avancado"),
       ),
-      body: FutureBuilder<List<Post>>(
-          future: _recuperarPostagens(),
-          builder: (context, snapshot) {
-            String result = "";
+      body: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(onPressed: _post, child: const Text("Salvar")),
+                  ElevatedButton(
+                      onPressed: _put, child: const Text("Atualizar")),
+                  ElevatedButton(
+                      onPressed: _delete, child: const Text("Remover"))
+                ],
+              ),
+              Expanded(
+                  child: FutureBuilder<List<Post>>(
+                      future: _recuperarPostagens(),
+                      builder: (context, snapshot) {
+                        String result = "";
 
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                break;
-              case ConnectionState.waiting:
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-                break;
-              case ConnectionState.active:
-                break;
-              case ConnectionState.done:
-                if (snapshot.hasError) {
-                  result = "Erro ao tentar carregar";
-                } else {
-                  return ListView.builder(
-                    itemCount: snapshot.data?.length,
-                    itemBuilder: (context, index) {
-                      List<Post>? lista = snapshot.data;
-                      Post post = lista![index];
+                        switch (snapshot.connectionState) {
+                          case ConnectionState.none:
+                            break;
+                          case ConnectionState.waiting:
+                            return const Center(
+                              child: CircularProgressIndicator(),
+                            );
+                            break;
+                          case ConnectionState.active:
+                            break;
+                          case ConnectionState.done:
+                            if (snapshot.hasError) {
+                              result = "Erro ao tentar carregar";
+                            } else {
+                              return ListView.builder(
+                                itemCount: snapshot.data?.length,
+                                itemBuilder: (context, index) {
+                                  List<Post>? lista = snapshot.data;
+                                  Post post = lista![index];
 
-                      return ListTile(
-                        title: Text(post.title),
-                        subtitle: Text(post.id.toString()),
-                      );
-                    },
-                  );
-                }
-                break;
-            }
+                                  return ListTile(
+                                    title: Text(post.title),
+                                    subtitle: Text(post.id.toString()),
+                                  );
+                                },
+                              );
+                            }
+                            break;
+                        }
 
-            return Center(
-              child: Text(result),
-            );
-          }),
+                        return Center(
+                          child: Text(result),
+                        );
+                      }))
+            ],
+          )),
     );
   }
 }
--- consumo_servico_avanc/lib/post.dart
+++ consumo_servico_avanc/lib/post.dart
@@ -6,27 +6,36 @@ class Post {
 
   Post(this._userId, this._id, this._title, this._body);
 
+  Map toJson() {
+    return {
+      "userId": this._userId,
+      "id": this._id,
+      "title": this._title,
+      "body": this._body
+    };
+  }
+
   String get body => _body;
 
-  set body(String value){
+  set body(String value) {
     _body = value;
   }
 
   String get title => _title;
 
-  set title(String value){
+  set title(String value) {
     _title = value;
   }
 
   int get id => _id;
 
-  set id(int value){
+  set id(int value) {
     _id = value;
   }
 
   int get userId => _userId;
 
-  set userId(int value){
+  set userId(int value) {
     _userId = value;
   }
 }
