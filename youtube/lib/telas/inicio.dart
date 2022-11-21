439e5039a5921498dbdd87ee609fd16dff1ce04c
JoaoCamposMBM <joao.pedro@mbmsolutions.com.br>
2022-11-21 08:32:58 -0300

    the end app youtube

--- youtube/lib/telas/inicio.dart
+++ youtube/lib/telas/inicio.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../api.dart';
 import '../model/video.dart';
+import 'package:flutter_youtube/flutter_youtube.dart';
 
 class Inicio extends StatefulWidget {
   String pesquisa;
@@ -36,18 +37,27 @@ class _InicioState extends State<Inicio> {
                     List<Video>? videos = snapshot.data;
                     Video video = videos![index];
 
-                    return Column(children: [
-                      Container(
-                          height: 200,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: NetworkImage(video.imagem!)))),
-                      ListTile(
-                        title: Text(video.titulo!),
-                        subtitle: Text(video.canal!),
-                      )
-                    ]);
+                    return GestureDetector(
+                      onTap: () {
+                        FlutterYoutube.playYoutubeVideoById(
+                            apiKey: CHAVE_YOUTUBE_API,
+                            videoId: video.id,
+                            autoPlay: true,
+                            fullScreen: true);
+                      },
+                      child: Column(children: [
+                        Container(
+                            height: 200,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(video.imagem!)))),
+                        ListTile(
+                          title: Text(video.titulo!),
+                          subtitle: Text(video.canal!),
+                        )
+                      ]),
+                    );
                   },
                   separatorBuilder: (constext, index) => const Divider(
                     height: 2,
