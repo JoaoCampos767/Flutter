00562d5f3feec6bafeb6298d3b5169d3f69e94f5
JoaoCamposMBM <joao.pedro@mbmsolutions.com.br>
2022-12-01 07:59:27 -0300

    melhorando a lista de tarefas

--- lista_tarefas/lib/home_page.dart
+++ lista_tarefas/lib/home_page.dart
@@ -11,19 +11,31 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List _listaTarefas = [];
+  TextEditingController _controllerTarefa = TextEditingController();
 
   Future<File> _getFile() async {
     final diretorio = await getApplicationDocumentsDirectory();
     return File("${diretorio.path}/dados.json");
   }
 
-  _salvarArquivo() async {
-    var arquivo = await _getFile();
+  _salvaTarefa() {
+    String textoDigitado = _controllerTarefa.text;
 
     Map<String, dynamic> tarefa = Map();
-    tarefa["titulo"] = "Ir trampar";
+    tarefa["titulo"] = textoDigitado;
     tarefa["realizada"] = false;
-    _listaTarefas.add(tarefa);
+
+    setState(() {
+      _listaTarefas.add(tarefa);
+    });
+
+    _salvarArquivo();
+
+    _controllerTarefa.text = "";
+  }
+
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
 
     String dados = json.encode(_listaTarefas);
     arquivo.writeAsString(dados);
@@ -48,10 +60,51 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Widget criarItemLista(context, index) {
+    final item = _listaTarefas[index]['titulo'];
+
+    return Dismissible(
+        key: Key(item),
+        direction: DismissDirection.endToStart,
+        onDismissed: (direction) {
+          _listaTarefas.removeAt(index);
+          // _salvarArquivo();
+
+          const snackbar = SnackBar(
+              duration: Duration(seconds: 5), content: Text("Tarefa removida"));
+
+          ScaffoldMessenger.of(context).showSnackBar(snackbar);
+        },
+        background: Container(
+          color: Colors.red,
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: const [
+              Icon(
+                Icons.delete,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+        child: CheckboxListTile(
+          title: Text(_listaTarefas[index]['titulo']),
+          value: _listaTarefas[index]['realizada'],
+          onChanged: (valorAlterado) {
+            setState(() {
+              _listaTarefas[index]['realizada'] = valorAlterado;
+            });
+
+            _salvarArquivo();
+          },
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    _salvarArquivo();
-    print(_listaTarefas.toString());
+    //_salvarArquivo();
+    //print(_listaTarefas.toString());
 
     return Scaffold(
       appBar: AppBar(
@@ -67,6 +120,7 @@ class _HomePageState extends State<HomePage> {
                 return AlertDialog(
                   title: const Text("Adicionar Tarefa"),
                   content: TextField(
+                    controller: _controllerTarefa,
                     decoration:
                         const InputDecoration(labelText: "Digite sua tarefa"),
                     onChanged: (text) {},
@@ -77,6 +131,7 @@ class _HomePageState extends State<HomePage> {
                         child: const Text("Cancelar")),
                     TextButton(
                         onPressed: () {
+                          _salvaTarefa();
                           Navigator.pop(context);
                         },
                         child: const Text("Salvar"))
@@ -91,11 +146,7 @@ class _HomePageState extends State<HomePage> {
         children: [
           Expanded(
               child: ListView.builder(
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(_listaTarefas[index]['titulo']),
-              );
-            },
+            itemBuilder: criarItemLista,
             itemCount: _listaTarefas.length,
           ))
         ],
