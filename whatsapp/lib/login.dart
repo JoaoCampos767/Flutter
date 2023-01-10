2faf76935b279881528b89323ed181533d2871bd
João Pedro Campos <102362798+JoaoCamposMBM@users.noreply.github.com>
2023-01-10 09:23:05 -0300

    login

---
+++ whatsapp/lib/login.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(color: Color(0xff075E54)),
+        padding: const EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 32),
+                  child: Image.asset(
+                    "images/logo.png",
+                    width: 150,
+                    height: 100,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: const TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "E-mail",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                ),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    hintText: "Senha",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 10),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    child: const Text(
+                      "Entrar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: const Text(
+                      "Não tem conta? Cadastre-se!",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- whatsapp/lib/main.dart
+++ whatsapp/lib/main.dart
@@ -1,16 +1,26 @@
 // @dart=2.9
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:whatsapp/home.dart';
+import 'package:whatsapp/login.dart';
 
-void main() {
+void main() async {
+  /*WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   FirebaseFirestore.instance
       .collection("usuarios")
       .doc("001")
-      .set({"nome": "Joao"});
+      .set({"nome": "Joao"});*/
 
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Home(),
+    home: const Login(),
+    theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: const Color(0xff075E54),
+            secondary: const Color(0xff25D366))),
   ));
 }
